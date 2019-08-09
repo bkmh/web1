220565e463bae02222542e94644244683f41a046
bkmh <wjsqhrhks@gmail.com>
2019-08-09 21:43:02 +0900

    생활코딩 express 예제 수행

--- myapp2/app.js
+++ myapp2/app.js
@@ -3,35 +3,107 @@ var express = require('express');
 
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var Path = require('path');
+// File Handling -> express not afford file handling
+var multer = require('multer');
+//var upload = multer({ dest: 'uploads/' });
+
+var _storage = multer.diskStorage({
+    // 사용자가 전송한 파일의 저장위치
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+
+    // 사용자 전송한 파일의 파일명
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+});
+
+
+var upload = multer({ storage: _storage });
 
 var app = express();
 
 app.locals.pretty = true;
+
+// static file 사용
+app.use('/user', express.static('uploads'));
+
 
 app.use(bodyParser.urlencoded({ exteneded: false }));
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.get('/topic/new', function(req,res){
-    res.render('new');
+// 파일 업로드
+app.get('/upload', function(req,res){
+    res.render('upload_form');
 });
 
-app.get('/topic', function(req,res){
-    fs.readdir('./data', function(err, files){
 
+// 해당 post 함수로 처리되기 전에 upload 즉, middleware인 multer가 수행된다.
+// single 하위의 parameter에는 multipart form의 name이 입력되어야 한다.
+app.post('/upload', upload.single('userFile'), function(req,res){
+    console.log(req.file);
+    res.send('Uploaded Complete : ' + req.file.filename);
+});
+
+
+// 동일한 주소에 대해 중복적으로 동일한 소스가 들어있으므로
+// 해당 부분에 대해서는 구조를 변경하여 동일하게 기능 제공
+
+app.get('/topic/new', function(req,res){
+
+    fs.readdir('./data/', function(err,files){
         if (err){
             console.log(err);
             res.status(500).send('Internal Server Error');
         }
 
-        console.log(files);
-
-        res.render('view', {topics:files});
-
+        res.render('new', {topics:files});
     });
 });
 
+
+//app.get('/topic', function(req,res){
+app.get(['/topic', '/topic/:id'], function(req,res){
+
+        var dataPath = './data/';
+
+        fs.readdir('./data/', function(err, files){
+    
+            if (err){
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            }
+    
+            var topic = req.params.id;
+    
+            if (topic) {
+                fs.readFile(dataPath + topic, {encoding: 'utf8'}, function(err, data){
+                    if (err) {
+                        console.log(err);
+                        res.status(500).send('Internal Server Error');
+                    }
+             
+                    res.render('view', {topics:files, title:topic, content:data});
+             
+                 });
+    
+                 
+            } else {
+                    res.render('view', {topics:files});
+            }
+    
+        });
+    });
+
+
+
+
+
+/* 중복제거를 위한 구조 변경 후 주석처리
 app.get('/topic/:id', function(req, res){
     
     var dataPath = './data/';
@@ -56,7 +128,7 @@ app.get('/topic/:id', function(req, res){
 
     });
 });
-
+*/
 
 app.post('/topic', function(req,res){
     var title = req.body.title;
@@ -70,8 +142,8 @@ app.post('/topic', function(req,res){
             res.status(500).send('Internal Server Error');
         }
         
-        console.log('File make Completed');
-        res.send('Hi, Post');
+        // 해당 경로를 다시 redirect 처리하는 방식
+        res.redirect('/topic/'+title);
 
     });
 
